09e8ba9cfdb2b4018c1d29a4328b00cd8c744fca
shengliang <shengl@elite-audio.cn>
2024-05-31 10:01:19 +0800

    D:去掉mock-server F:修复一个报错

--- vue.config.js
+++ vue.config.js
@@ -37,7 +37,7 @@ module.exports = {
       errors: true,
     },
     // lintOnSave: false,
-    before: require("./mock/mock-server.js"),
+    // before: require("./mock/mock-server.js"),
   },
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
